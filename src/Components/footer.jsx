fa533cdecccdfb5cc0f189cee45d6dabba55ed74
FaZil-shaik <fazilshaik103@gmail.com>
2024-02-06 10:47:24 +0530

    second commit

---
+++ src/Components/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css'
+const Footer = () =>{
+    return(
+<>
+      <h1 className='kka'>Shortnered URL is As shown Above 👆🏼</h1>
+</>
+    )
+}
+
+export default Footer
---
+++ src/Components/header.jsx
@@ -0,0 +1,13 @@
+import "./page.css";
+
+const Header = () =>{
+    return(
+        <>
+        <div className="heading">
+            <div>URL-SHORTNER</div>
+        </div>
+        </>
+    )
+}
+
+export default Header;
